a6b188121eeeec4f4cb2378e478d2b2aae9f0736
SpectrixBruh <owenhutch123@gmail.com>
2022-03-23 18:11:26 +0000

    Fixed f-string bug

--- phoneBot.py
+++ phoneBot.py
@@ -1,5 +1,8 @@
-""" For setup instructions, read the README.md
-By Spectrix#0001. Enjoy """
+""" 
+For setup instructions, read the README.md
+By Spectrix. Enjoy
+https://spectrixdev.github.io/
+"""
 
 import discord, asyncio, aiohttp, json
 from discord.ext import commands
@@ -12,7 +15,7 @@ bot = commands.Bot(command_prefix=commands.when_mentioned_or(config["prefix"]), 
 
 @bot.event
 async def on_ready():
-    print("=========\nReady for use!\n=========\nBot created by Spectrix#0001! hf <3")
+    print("=========\nReady for use!\n=========\nBot created by Spectrix#7745! hf <3")
 
 @commands.cooldown(1, int(config["callCoolDown"]), BucketType.user)
 @bot.command()
@@ -22,7 +25,7 @@ async def call(ctx, *, message):
         report["value1"] = f"New message. {message}"
         report["value2"] = f". Sent by {ctx.author.name} on the server {ctx.guild.name}."
         async with aiohttp.ClientSession() as session:
-            await session.post(f"https://maker.ifttt.com/trigger/{config["eventName"]}/with/key/{config["IFTTTkey"]}", data=report)
+            await session.post(f"https://maker.ifttt.com/trigger/{config['eventName']}/with/key/{config['IFTTTkey']}", data=report)
         await ctx.send("**Data posted! Calling the bot owner now :telephone_receiver:**")
     else:
         await ctx.send(f"**{ctx.author.mention} You can't send messages over 250 chars long. :no_entry:**")
@@ -33,5 +36,4 @@ async def on_command_error(ctx, error):
         await ctx.send(f"**:no_entry: {error}**")
 
 if __name__ == '__main__':
-
     bot.run(config["discordToken"])
